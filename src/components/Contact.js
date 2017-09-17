7a6ff4145cccbf89f894a3574f0387375d80283e
Rudy Gonzalez <xrgonzalezx@gmail.com>
2017-09-17 03:16:00 -0400

    updated component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,13 +19,13 @@ const Contact = () => {
       <h1>Contact <span style={{fontFamily: "Times", color:"#C1C2C7", fontSize:"1.7em"}}> Hurricane Start</span> At</h1>
       <br/>
       <ul className="col-4 offset-5" style={listStyle}>
-        <li style={contactStyle}>Email</li>
+        <li className="fa fa-envelope" aria-hidden="true"style={contactStyle}></li>
         <li>hurricaneStart@gmail.com</li>
         <br/>
-        <li style={contactStyle}>Telephone</li>
+        <li className="fa fa-phone" aria-hidden="true" style={contactStyle}></li>
         <li>800-646-4321</li>
         <br/>
-        <li style={contactStyle}>Address</li>
+        <li className="fa fa-home" aria-hidden="true" color="blue" style={contactStyle}></li>
         <li>300 Spartan St</li>
         <li>Bronx, NY 37067</li>
       </ul>
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -8,7 +8,7 @@ const Detail = (props) => {
     <div className="col-sm-6">
       <div className="card " style={{marginTop: 20}}>
         <div className="card-block">
-          <img className="card-image-top" src={detail.image} style={{width:"250px", float:"right"}} />
+          <img className="card-image-top" src={detail.image} style={{width:"250px", float:"right", height:"250px"}} />
           <h4 className="card-title">{detail.name}</h4>
           <p className="card-text">Order-Number: {detail.orderNumber}</p>
           <ul className="list-group list-group">
